ee860a2d8c1df65e085653c67cf3ce7d857f73c3
mtemmar <mfa.temmar@gmail.com>
2019-04-02 09:52:45 +0200

    added test assignment
    tests/Unit/VoorbeeldTest

---
+++ tests/Unit/VoorbeeldTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class VoorbeeldTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+//    public function testExample()
+//    {
+//        $this->assertTrue(true);
+//    }
+
+/*
+    public function testIndexViewHasAvailableAssignments(){
+        $response = $this->get('/assignments');
+
+        $response->assertStatus(200);
+        $response->assertViewIs('assignment.index');
+        $response->assertViewHas('assignments');
+    }
+//*/
+    //**
+    public function testItWritesGoodFormToDatabaseAndRedirectsToIndex(){
+        $token = 'covfefe';
+        $name = 'Stomme cursus';
+        $description = 'fet stome cursus';
+
+        $response = $this
+            ->withSession(['_token' => $token])
+            ->post(
+                '/assignments',
+                [
+                    '_token' => $token,
+                    'naamInput' => $name,
+                    'beschrijvingTextArea' => $description,
+                ]
+            );
+
+        $this->assertDatabaseHas(
+            'assignments',
+            [
+                'naam' => $name,
+                'beschrijving' => $description
+            ]
+        );
+
+        $response->assertStatus(302);
+        $response->assertRedirect('assignments');
+    }
+
+     //*/
+}
